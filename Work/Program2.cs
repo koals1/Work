ec93a8d9cc04e5f42fc8783ebd30dcdf83fdd42e
ASUS <ASUS@DESKTOP-7ONLO00>
2025-10-15 13:44:47 +0300

    Client and Server

---
+++ Work/Program2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpChat
+{
+    class Server
+    {
+        private readonly UdpClient socket;
+        private readonly ConcurrentDictionary<IPEndPoint, string> users = new();
+
+        public Server(int port)
+        {
+            socket = new UdpClient(port);
+            Console.WriteLine($"Server is listening on port {port}");
+        }
+
+        public async Task RunAsync()
+        {
+            while (true)
+            {
+                var packet = await socket.ReceiveAsync();
+                string msg = Encoding.UTF8.GetString(packet.Buffer);
+                var sender = packet.RemoteEndPoint;
+
+                if (!users.ContainsKey(sender))
+                {
+                    users[sender] = msg;
+                    Console.WriteLine($"User joined: {msg} ({sender})");
+                    continue;
+                }
+
+                string name = users[sender];
+                string finalMsg = $"{name}: {msg}";
+                Console.WriteLine(finalMsg);
+
+                byte[] data = Encoding.UTF8.GetBytes(finalMsg);
+                foreach (var u in users.Keys)
+                    await socket.SendAsync(data, data.Length, u);
+            }
+        }
+
+        static async Task Main()
+        {
+            var server = new Server(8888);
+            await server.RunAsync();
+        }
+    }
+}
---
+++ Work/Program3.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpChat
+{
+    class Client
+    {
+        private readonly UdpClient client;
+        private readonly IPEndPoint serverEndpoint;
+        private readonly string username;
+
+        public Client(string name, string host, int port)
+        {
+            username = name;
+            serverEndpoint = new IPEndPoint(IPAddress.Parse(host), port);
+            client = new UdpClient();
+        }
+
+        public async Task StartAsync()
+        {
+            await SendAsync(username);
+            _ = ReceiveMessagesAsync();
+
+            Console.WriteLine("Connected. Type messages below:");
+            while (true)
+            {
+                string? line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                await SendAsync(line);
+            }
+        }
+
+        private async Task SendAsync(string text)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            await client.SendAsync(data, data.Length, serverEndpoint);
+        }
+
+        private async Task ReceiveMessagesAsync()
+        {
+            while (true)
+            {
+                var msg = await client.ReceiveAsync();
+                Console.WriteLine(Encoding.UTF8.GetString(msg.Buffer));
+            }
+        }
+
+        static async Task Main()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine() ?? "User";
+            var c = new Client(name, "127.0.0.1", 8888);
+            await c.StartAsync();
+        }
+    }
+}
