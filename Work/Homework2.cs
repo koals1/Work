56d6f5e1a9c27004965581f16c459989d333b673
ASUS <ASUS@DESKTOP-7ONLO00>
2025-11-19 14:58:54 +0200

    js

---
+++ Work/Homework2.cs
@@ -0,0 +1,37 @@
+﻿// 1 --------------------------------
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+function compare(a, b)
+{
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+}
+
+// 2 --------------------------------
+function factorial(n)
+{
+    let r = 1;
+    for (let i = 1; i <= n; i++) r *= i;
+    return r;
+}
+
+// 3 --------------------------------
+function makeNumber(a, b, c)
+{
+    return +(`${ a}${ b}${ c}`);
+}
+
+// 4 --------------------------------
+function area(a, b)
+{
+    return b === undefined ? a * a : a * b;
+}
+
+// 5 --------------------------------
+function isPerfect(n)
+{
+    let s = 0;
+    for (let i = 1; i < n; i++) if (n % i === 0) s += i;
+    return s === n;
+}
