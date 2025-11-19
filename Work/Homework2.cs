dfb67d2637c9d360fae4b3f1d12a073d44da7cd3
ASUS <ASUS@DESKTOP-7ONLO00>
2025-11-19 15:12:34 +0200

    js

--- Work/Homework2.cs
+++ Work/Homework2.cs
@@ -1,36 +1,36 @@
-﻿// 1 --------------------------------
+﻿//// 1 --------------------------------
 
-function compare(a, b)
-{
-    if (a < b) return -1;
-    if (a > b) return 1;
-    return 0;
-}
+//function compare(a, b)
+//{
+//    if (a < b) return -1;
+//    if (a > b) return 1;
+//    return 0;
+//}
 
-// 2 --------------------------------
-function factorial(n)
-{
-    let r = 1;
-    for (let i = 1; i <= n; i++) r *= i;
-    return r;
-}
+//// 2 --------------------------------
+//function factorial(n)
+//{
+//    let r = 1;
+//    for (let i = 1; i <= n; i++) r *= i;
+//    return r;
+//}
 
-// 3 --------------------------------
-function makeNumber(a, b, c)
-{
-    return +(`${ a}${ b}${ c}`);
-}
+//// 3 --------------------------------
+//function makeNumber(a, b, c)
+//{
+//    return +(`${ a}${ b}${ c}`);
+//}
 
-// 4 --------------------------------
-function area(a, b)
-{
-    return b === undefined ? a * a : a * b;
-}
+//// 4 --------------------------------
+//function area(a, b)
+//{
+//    return b === undefined ? a * a : a * b;
+//}
 
-// 5 --------------------------------
-function isPerfect(n)
-{
-    let s = 0;
-    for (let i = 1; i < n; i++) if (n % i === 0) s += i;
-    return s === n;
-}
+//// 5 --------------------------------
+//function isPerfect(n)
+//{
+//    let s = 0;
+//    for (let i = 1; i < n; i++) if (n % i === 0) s += i;
+//    return s === n;
+//}
---
+++ Work/Homework3.cs
@@ -0,0 +1,99 @@
+﻿// 1---------------------
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+using System;
+
+let shoppingList = [
+  { name: "Хліб", qty: 1, bought: false },
+  { name: "Молоко", qty: 2, bought: true },
+  { name: "Яйця", qty: 10, bought: false }
+];
+
+function showList()
+{
+    for (let i = 0; i < shoppingList.length; i++)
+    {
+        if (!shoppingList[i].bought) console.log(shoppingList[i]);
+    }
+    for (let i = 0; i < shoppingList.length; i++)
+    {
+        if (shoppingList[i].bought) console.log(shoppingList[i]);
+    }
+}
+
+function addItem(name, qty)
+{
+    let found = false;
+    for (let i = 0; i < shoppingList.length; i++)
+    {
+        if (shoppingList[i].name === name)
+        {
+            shoppingList[i].qty += qty;
+            found = true;
+            break;
+        }
+    }
+    if (!found) shoppingList.push({ name: name, qty: qty, bought: false });
+}
+
+function buyItem(name)
+{
+    for (let i = 0; i < shoppingList.length; i++)
+    {
+        if (shoppingList[i].name === name)
+        {
+            shoppingList[i].bought = true;
+            break;
+        }
+    }
+}
+
+// 2---------------------
+
+let receipt = [
+  { name: "Хліб", qty: 1, price: 20 },
+  { name: "Молоко", qty: 2, price: 35 },
+  { name: "Сир", qty: 1, price: 90 }
+];
+
+function showReceipt()
+{
+    for (let i = 0; i < receipt.length; i++)
+    {
+        console.log(receipt[i].name, receipt[i].qty, receipt[i].price);
+    }
+}
+
+function totalSum()
+{
+    let sum = 0;
+    for (let i = 0; i < receipt.length; i++)
+    {
+        sum += receipt[i].qty * receipt[i].price;
+    }
+    return sum;
+}
+
+function mostExpensive()
+{
+    let max = receipt[0];
+    for (let i = 1; i < receipt.length; i++)
+    {
+        if (receipt[i].price * receipt[i].qty > max.price * max.qty)
+        {
+            max = receipt[i];
+        }
+    }
+    return max;
+}
+
+function averageItemCost()
+{
+    let totalItems = 0;
+    for (let i = 0; i < receipt.length; i++)
+    {
+        totalItems += receipt[i].qty;
+    }
+    return totalSum() / totalItems;
+}
